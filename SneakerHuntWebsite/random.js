da3e38bf089d6155ba5fa34aee7345630636cb5e
loodsD <117138290+loodsD@users.noreply.github.com>
2023-01-20 11:59:32 +0800

    Add files via upload

---
+++ SneakerHuntWebsite/random.js
@@ -0,0 +1,178 @@
+const menSneakers = [
+     "imgsrc/nike/male/Nike Air Force 1 Mid React.PNG",
+     "imgsrc/nike/male/Air Jordan 1 Mid SE.PNG",
+     "imgsrc/nike/male/Nike Air Max Terrascape 97.PNG",
+     "imgsrc/nike/male/Nike Air Max Terrascape Plus.PNG",
+     "imgsrc/nike/male/Nike Blazer Mid '77 Jumbo.jpeg",
+     "imgsrc/adidas/male/FORCEBOUNCE VOLLEYBALL SHOES.jpg",
+     "imgsrc/adidas/male/LITE RACER ADAPT 4.0 SHOES.png",
+     "imgsrc/adidas/male/LITE RACER ADAPT 5.0 SHOES.jpg",
+     "imgsrc/adidas/male/SWIFT RUN SHOES.png",
+     "imgsrc/adidas/male/ULTRABOOST 1.0 SHOES.png",
+     "imgsrc/vans/male/CosmicCheckOldSkool_1.jpg",
+     "imgsrc/vans/male/CrayolaComfycushOldSkool_1.jpg",
+     "imgsrc/vans/male/JungleClash_01.jpg",
+     "imgsrc/vans/male/blackclassic_OldSkool_1.jpg",
+     "imgsrc/vans/male/mintclassic_OldSkool_1.jpg",
+     "imgsrc/puma/male/Blaze of Glory (Reverse Classics) Trainers .jpg",  //puma
+     "imgsrc/puma/male/PUMA Relax Men's Slip-On Shoes.jpg",
+     "imgsrc/puma/male/Puma Fire V2 Men's Shoes.jpg",
+     "imgsrc/puma/male/RS-Z Trainers.jpg",
+     "imgsrc/puma/male/Smash v2 Trainers.jpg"
+];
+
+const maleSneakerName = [
+     "Nike Air Force 1 Mid React",
+     "Nike Air Jordan 1 Mid SE",
+     "Nike Air Max Terrascape 97",
+     "Nike Air Max Terrascape Plus",
+     "Nike Blazer Mid '77 Jumbo",
+     "ADIDAS FORCEBOUNCE VOLLEYBALL SHOES",
+     "ADIDAS LITE RACER ADAPT 4.0 SHOES",
+     "ADIDAS LITE RACER ADAPT 5.0 SHOES",
+     "ADIDAS SWIFT RUN SHOES",
+     "ADIDAS ULTRABOOST 1.0 SHOES",
+     "Vans CosmicCheckOldSkool",
+     "Vans CrayolaComfycushOldSkool",
+     "Vans JungleClash",
+     "Vans Blackclassic OldSkool",
+     "Vans Mintclassic OldSkool",
+     "Puma Blaze of Glory (Reverse Classics) Trainers",
+     "Puma Relax Men's Slip-On Shoes",
+     "iPuma Fire V2 Men's Shoes",
+     "Puma RS-Z Trainers",
+     "Puma Smash v2 Trainers"
+];
+const linkMale = [
+     "sneakerFile/sneaker23.html",
+     "sneakerFile/sneaker25.html",
+     "sneakerFile/sneaker22.html",
+     "sneakerFile/sneaker24.html",
+     "sneakerFile/sneaker21.html",
+     "sneakerFile/sneaker1.html",
+     "sneakerFile/sneaker4.html",
+     "sneakerFile/sneaker2.html",
+     "sneakerFile/sneaker5.html",
+     "sneakerFile/sneaker3.html",
+     "sneakerFile/sneaker12.html",
+     "sneakerFile/sneaker13.html",
+     "sneakerFile/sneaker14.html",
+     "sneakerFile/sneaker11.html",
+     "sneakerFile/sneaker15.html",
+     "sneakerFile/sneaker39.html",
+     "sneakerFile/sneaker36.html",
+     "sneakerFile/sneaker37.html",
+     "sneakerFile/sneaker40.html",
+     "sneakerFile/sneaker38.html",
+     //add Puma Male sneaker HTML directory here...
+];
+
+const womenSneakers = [
+     "imgsrc/nike/female/Air Jordan 1 Elevate High.PNG",
+     "imgsrc/nike/female/Nike AF-1 Shadow.PNG",
+     "imgsrc/nike/female/Nike Air Max Flyknit Racer.PNG",
+     "imgsrc/nike/female/Nike Air Max Koko.PNG",
+     "imgsrc/nike/female/Nike SuperRep Cycle 2 Next Nature.PNG",
+     "imgsrc/adidas/female/GRAND COURT CLOUDFOAM LIFESTYLE COURT COMFORT SHOES.png",
+     "imgsrc/adidas/female/MULTIX SHOES.png",
+     "imgsrc/adidas/female/NMD_R1 SHOES.png",
+     "imgsrc/adidas/female/ULTRA 4DFWD RUNNING SHOES.png",
+     "imgsrc/adidas/female/ULTRABOOST DNA SHOES.png",
+     "imgsrc/vans/female/SLIPON_3.jpg",
+     "imgsrc/vans/female/Skateclassic_1.jpg",
+     "imgsrc/vans/female/VANSXONEPIECE_1.jpg",
+     "imgsrc/vans/female/VansxDanielJohnston_1.jpg",
+     "imgsrc/vans/female/VANSXSPONGEBOB_1.jpg",
+     "imgsrc/puma/female/Harrow Women's Shoes .jpg",
+     "imgsrc/puma/female/Muse Metal Women Trainers.jpg",
+     "imgsrc/puma/female/PUMA x AMI Rider FV Sneakers.jpg",
+     "imgsrc/puma/female/PUMA x POKÉMON Rider FV Bulbasaur Sneakers.jpg",
+     "imgsrc/puma/female/PUMA x POKEMON Rider FV Pikachu Sneakers.jpg"
+];
+
+const womenSneakerName = [
+    "Nike Air Jordan 1 Elevate High",
+    "Nike AF-1 Shadow",
+    "Nike Air Max Flyknit Racer",
+    "Nike Air Max Koko",
+    "Nike SuperRep Cycle 2 Next Nature",
+    "ADIDAS GRAND COURT CLOUDFOAM LIFESTYLE COURT COMFORT SHOES",
+    "ADIDAS MULTIX SHOES",
+    "ADIDAS NMD_R1 SHOES",
+    "ADIDAS ULTRA 4DFWD RUNNING SHOES",
+    "ADIDAS ULTRABOOST DNA SHOES",
+    "Vans SlipOn",
+    "Vans Skateclassic",
+    "Vans X One Piece",
+    "Vans X Daniel Johnston", 
+    "Vans X Spongebob",
+    "Puma Harrow Women's Shoes",
+    "Puma Muse Metal Women Trainers",
+    "PUMA x AMI Rider FV Sneakers",
+    "PUMA x POKEMON Rider FV Bulbasaur Sneakers",
+    "PUMA x POKEMON Rider FV Pikachu Sneakers"
+];
+
+const linkWomen = [
+     "sneakerFile/sneaker26.html",
+     "sneakerFile/sneaker29.html",
+     "sneakerFile/sneaker27.html",
+     "sneakerFile/sneaker28.html",
+     "sneakerFile/sneaker30.html",
+     "sneakerFile/sneaker10.html",
+     "sneakerFile/sneaker6.html",
+     "sneakerFile/sneaker9.html",
+     "sneakerFile/sneaker8.html",
+     "sneakerFile/sneaker7.html",
+     "sneakerFile/sneaker17.html",
+     "sneakerFile/sneaker16.html",
+     "sneakerFile/sneaker19.html",
+     "sneakerFile/sneaker18.html",
+     "sneakerFile/sneaker20.html",
+     "sneakerFile/sneaker35.html",
+     "sneakerFile/sneaker32.html",
+     "sneakerFile/sneaker31.html",
+     "sneakerFile/sneaker33.html",
+     "sneakerFile/sneaker34.html",
+
+     //add puma women sneakers HTML directory here...
+];
+
+var gender = "";
+function verifyGender(){
+    var male = document.getElementById("men");  
+  var female = document.getElementById("women"); 
+  if(male.checked == false && female.checked == false) {
+     alert("Please refresh this page and select a gender. 😊");
+  }
+   if(female.checked == true){
+        gender = "women";
+   }else if(male.checked == true){
+        gender="men"
+   }
+   return gender;
+}
+
+function displayImage(){   
+     var male = document.getElementById("men");  
+     var female = document.getElementById("women"); 
+     if(male.checked == false && female.checked == false) {
+        alert("Please refresh this page and select a gender. 😊");
+     }
+    var pic = "";
+    var desc = "";
+    var url = "";
+    if(verifyGender() == "women"){
+         pic = womenSneakers;
+         desc = womenSneakerName;
+         url = linkWomen;
+    }else if(verifyGender() == "men"){
+     pic = menSneakers;
+     desc = maleSneakerName;
+     url = linkMale;
+    }
+    var i = Math.floor(Math.random()*pic.length);
+    document.getElementById("name").innerHTML = 'Get yourself a pair of '+desc[i]+'! 😍';
+    document.getElementById("sneaker").src = pic[i];
+    document.getElementById("link").href = url[i];
+}
